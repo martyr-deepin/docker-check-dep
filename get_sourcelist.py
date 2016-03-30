aeee3f692939a8138dc62719fa19b5d2e6bbce73
amazingfate <liujianfeng1994@gmail.com>
2016-03-30 16:18:29 +0800

    remove pycurl

--- get_sourcelist.py
+++ get_sourcelist.py
@@ -7,16 +7,6 @@ from io import BytesIO
 RR_URL = 'https://rr.deepin.io/api/v1/review/%s'
 
 def get_repo_review(review_id):
-    buffer = BytesIO()
-    url = RR_URL % review_id
-    pycurl_connect = pycurl.Curl()
-    pycurl_connect.setopt(pycurl.URL, url)
-    pycurl_connect.setopt(pycurl.HTTPHEADER, ['Access-Token:x'])
-    pycurl_connect.setopt(pycurl.WRITEDATA, buffer)
-    pycurl_connect.perform()
-    pycurl_connect.close()
-    body = buffer.getvalue()
-    data = json.loads(body.decode('utf-8'))
     base = data['result']['base']
     base_codename = data['result']['base_codename']
     rpa = data['result']['rpa']
