34a69cb3123d9fa8782dcc89aaa332924269ef38
amazingfate <liujianfeng1994@gmail.com>
2016-03-30 16:23:06 +0800

    remove unused option

--- checkdep.sh
+++ checkdep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get -y install python python-apt coreutils python-pycurl
-python /docker-check-dep/get_sourcelist.py $1
+python /docker-check-dep/get_sourcelist.py
 rm /etc/apt/sources.list
 cp sources.list /etc/apt/
 apt-get -y update
