e56479ec723f3b3f5646a990480a022730be5d4c
amazingfate <liujianfeng1994@gmail.com>
2016-04-14 10:08:14 +0800

    show result

--- get_dep_details.sh
+++ get_dep_details.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-echo "details of depend issues:" > result.log
 if [ -f $PWD/output-$REVIEW_ID ]
 then
+    echo "details of depend issues:" > result.log
     while read a
     do
         if ! apt-get --dry-run install $a > /dev/null
         then
             echo $a >> result.log
-            apt-get --dry-run install $a |grep "Depends:" >> result.log
+            apt-get --dry-run install $a 2>&1|grep "Depends:" >> result.log
             echo " " >> result.log
         fi
     done < output-$REVIEW_ID
+else
+    echo "no issues found" > result.log
 fi
+cat result.log
