64944a85a78a830a08817446958d350da3e4bbf5
amazingfate <liujianfeng1994@gmail.com>
2016-04-15 13:50:33 +0800

    simplify curl script

--- curl_back.sh
+++ curl_back.sh
@@ -8,12 +8,12 @@ BUILD_URL=$4
 if [[ ${cmd_res} == 0 ]]; then
 if cat $PWD/result.log|grep "Depends"
 then
-curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=check dep not pass.\njob details: <a target=\"_blank\" href=\"${BUILD_URL}/console\">${BUILD_URL}</a> "
+curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=check dep not pass.\njob details: ${BUILD_URL}/console"
 rm $PWD/result.log
 else
-curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=1&comment=check dep pass.\njob details: <a target=\"_blank\" href=\"${BUILD_URL}/console\">${BUILD_URL}</a> "
+curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=1&comment=check dep pass.\njob details: ${BUILD_URL}console"
 fi
 else
-	curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=check dep failed.\njob details: <a target=\"_blank\" href=\"${BUILD_URL}/console\">${BUILD_URL}</a> "
+	curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=check dep failed.\njob details: ${BUILD_URL}console"
 fi
 rm record.rd || echo "record.rd not found"
