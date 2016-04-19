519675af3bc97f8c53d7525789d2660d3671aaf2
amazingfate <liujianfeng1994@gmail.com>
2016-04-19 10:30:21 +0800

    change comment to Chinese

--- curl_back.sh
+++ curl_back.sh
@@ -8,12 +8,12 @@ BUILD_URL=$4
 if [[ ${cmd_res} == 0 ]]; then
 if cat $PWD/result.log|grep "Depends"
 then
-curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=check dep not pass.\njob details: ${BUILD_URL}console"
+curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=依赖检测未通过.\njob details: ${BUILD_URL}console"
 rm $PWD/result.log
 else
-curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=1&comment=check dep pass.\njob details: ${BUILD_URL}console"
+curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=1&comment=依赖检测通过.\njob details: ${BUILD_URL}console"
 fi
 else
-	curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=check dep failed.\njob details: ${BUILD_URL}console"
+	curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=依赖检测失败.\njob details: ${BUILD_URL}console"
 fi
 rm record.rd || echo "record.rd not found"
