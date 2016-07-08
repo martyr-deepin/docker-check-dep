a5e5f6106d982af08d1e4cec52cd0639ce06a51e
amazingfate <liujianfeng1994@gmail.com>
2016-07-08 16:03:14 +0800

    add pipeline support

--- curl_back.sh
+++ curl_back.sh
@@ -4,16 +4,19 @@ cmd_res=$1
 host_api=$2
 review_id=$3
 BUILD_URL=$4
+patch_set=`curl -H Access-Token:${CHECK_TOKEN} ${host_api}/review/${review_id}|json_pp|grep latest_patch_set|sed 's/,//'|sed 's/"latest_patch_set" : //g'`
 
 if [[ ${cmd_res} == 0 ]]; then
 if cat $PWD/result.log|grep "Depends"
 then
 curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=依赖检测未通过.\n`cat $PWD/final.log`\njob details: ${BUILD_URL}console"
+curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/checker/result -d "review_id=${review_id}&patch_set=${patch_set}&link=${BUILD_URL}&passed=0"
 rm $PWD/result.log
 rm $PWD/resultpkg.log || echo "resultpkg.log not found"
 rm $PWD/final.log || echo "final.log not found"
 else
 curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=1&comment=依赖检测通过.\njob details: ${BUILD_URL}console"
+curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/checker/result -d "review_id=${review_id}&patch_set=${patch_set}&link=${BUILD_URL}&passed=1"
 fi
 else
 	curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/test_result/${review_id} -d "passed=0&comment=依赖检测失败.\njob details: ${BUILD_URL}console"
--- curl_start.sh
+++ curl_start.sh
@@ -2,4 +2,5 @@
 host_api=$1
 review_id=$2
 BUILD_URL=$3
-curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/comment/${review_id} -d "score=0&verified=0&content=check dep started.\njob details: <a target=\"_blank\" href=\"${BUILD_URL}/console\">${BUILD_URL}</a> "
+patch_set=`curl -H Access-Token:${CHECK_TOKEN} ${host_api}/review/${review_id}|json_pp|grep latest_patch_set|sed 's/,//'|sed 's/"latest_patch_set" : //g'`
+curl -X POST -H Access-Token:${CHECK_TOKEN} ${host_api}/checker/launch -d "review_id=${review_id}&patch_set=${patch_set}"
